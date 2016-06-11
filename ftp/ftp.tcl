6da4b559ae5a23bcb9036d6dfe5de9fc4facd12d
unknown <minh.lq7@gmail.com>
2016-06-12 04:49:45 +0700

    abc

--- ftp/ftp.tcl
+++ ftp/ftp.tcl
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 set user "anonymous"
-spawn ftp -i 10.23.144.140
+spawn ftp -i 10.23.144.140      # -i option to download multiple files without being asked to input y for each file
 expect "Name"
 send "$user\r"
 expect "Password"
